d082f1bf8ac2aabdfb05077226ac68153035347d
AM-TRAINER <amazmishvili1@gmail.com>
2024-11-28 00:21:46 +0400

    Add files via upload

---
+++ translate.js
@@ -0,0 +1,8 @@
+
+        //translation
+        function googleTranslateElementInit() {
+            new google.translate.TranslateElement(
+                { pageLanguage: 'ka', includedLanguages: 'en,ru,de', layout: google.translate.TranslateElement.InlineLayout.SIMPLE },
+                'google_translate_element'
+            );
+        }
